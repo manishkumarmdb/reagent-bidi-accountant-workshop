9660d890233bec8e0e37f94d2d362b2ec2dd55f5
manishkumarmdb <manishkumarmdb@gmail.com>
2016-08-09 13:46:28 +0530

    update book-list function

--- src/cljs/cljs_boot_starter/bookslist.cljs
+++ src/cljs/cljs_boot_starter/bookslist.cljs
@@ -1,27 +1,20 @@
 (ns cljs-boot-starter.bookslist)
 
 (defonce author-and-books
-  [{:author "Thomas H. Cormen" :books ["Introduction to Algorithms" "Algorithms Unlocked"]}
+  [{:author "Thomas H. Cormen" :books ["Introduction to Algorithms"
+                                       "Algorithms Unlocked"]}
    {:author "Narasimha Karumanchi" :books ["Data Structures and Algorithms Made Easy"]}
-   {:author "Robert Sedgewick" :books ["Algorithms, 4th edition" "An Introduction to Programming in Java" "An Introduction to Computer Science"]}]
+   {:author "Robert Sedgewick" :books ["Algorithms, 4th edition"
+                                       "An Introduction to Programming in Java"
+                                       "An Introduction to Computer Science"]}]
   )
 
 (defn book-list []
   [:div
-   [:table.table
+   [:table
     [:thead>tr
      [:th "Author's Name"]
      [:th "Book's Name"]]
-    #_[:tbody
-       [:tr
-        [:td "1"]
-        [:td "2"]]
-       [:tr.tr
-        [:td "3"]
-        [:td "4"]]
-       [:tr
-        [:td "5"]
-        [:td "6"]]]
     [:tbody
      (for [v author-and-books]
        ^{:key (:author v)}
@@ -29,5 +22,10 @@
         [:td
          (:author v)]
         [:td
-         (get (:books v) 0)]])]
+         (for [b (range (count (:books v)))]
+           ^{:key b}
+           [:li
+            (-> (:books v)
+                (get b))])]
+        ])]
     ]])
