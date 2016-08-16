767d3ef417a56c9eecc9122f634b14fad080191e
manishkumarmdb <manishkumarmdb@gmail.com>
2016-08-16 16:42:31 +0530

    update UI

--- src/cljs/cljs_boot_starter/signin.cljs
+++ src/cljs/cljs_boot_starter/signin.cljs
@@ -4,16 +4,19 @@
 (defn sign-in []
   [:div
    [:form
-    [:div
-     [:label " username/email : "]
-     [comp/input {:type "text"
-                  :placeholder "enter your username/email"
-                  :name "username"}]]
-    [:div
-     [:label " password : "]
-     [comp/input {:type "password"
-                  :placeholder "enter your password"
-                  :name "password"}]]
-    [:div
-     [:label " "]
-     [comp/input {:type "submit"} "Sign in"]]]])
+    [:table.signin-table
+     #_[:thead>tr
+        [:th.signin-th "username/email"]
+        [:th.signin-th "password"]]
+     [:tbody
+      [:tr
+       [:td
+        [comp/input {:type "text"
+                     :placeholder "enter your username/email"
+                     :name "username"}]]
+       [:td
+        [comp/input {:type "password"
+                     :placeholder "enter your password"
+                     :name "password"}]]
+       [:td
+        [comp/input {:type "submit"} "Sign in"]]]]]]])
