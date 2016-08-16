2606bec5451c35db2acf66aff120d4abfce4fb54
manishkumarmdb <manishkumarmdb@gmail.com>
2016-08-16 16:42:47 +0530

    update UI

--- src/cljs/cljs_boot_starter/signup.cljs
+++ src/cljs/cljs_boot_starter/signup.cljs
@@ -4,31 +4,39 @@
 (defn sign-up []
   [:div
    [:form
-    [:div
-     [:label " First Name : "]
-     [comp/input {:type "text"
-                  :placeholder "enter your first name"
-                  :name "firstname"}]]
-    [:div
-     [:label " Last Name : "]
-     [comp/input {:type "text"
-                  :placeholder "enter your last name"
-                  :name "lastname"}]]
-    [:div
-     [:label " Email : "]
-     [comp/input {:type "text"
-                  :placeholder "enter valid email id"
-                  :name "emailid"}]]
-    [:div
-     [:label " Password : "]
-     [comp/input {:type "password"
-                  :placeholder "enter any password"
-                  :name "password"}]]
-    [:div
-     [:label " Re-enter Password : "]
-     [comp/input {:type "password"
-                  :placeholder "re-enter above password"
-                  :name "password"}]]
-    [:div
-     [:label " "]
-     [comp/input {:type "submit"} "Submit"]]]])
+    [:table.signup-table
+     [:tbody
+      [:tr
+       [:td " First Name : "]
+       [:td
+        [comp/input {:type "text"
+                     :placeholder "enter your first name"
+                     :name "firstname"}]]]
+      [:tr
+       [:td " Last Name : "]
+       [:td
+        [comp/input {:type "text"
+                     :placeholder "enter your last name"
+                     :name "lastname"}]]]
+      [:tr
+       [:td " Email : "]
+       [:td
+        [comp/input {:type "text"
+                     :placeholder "enter valid email id"
+                     :name "emailid"}]]]
+      [:tr
+       [:td " Password : "]
+       [:td
+        [comp/input {:type "password"
+                     :placeholder "enter any password"
+                     :name "password"}]]]
+      [:tr
+       [:td " Re-enter Password : "]
+       [:td
+        [comp/input {:type "password"
+                     :placeholder "re-enter above password"
+                     :name "password"}]]]
+      [:tr
+       [:td ""]
+       [:td
+        [comp/input {:type "submit"} "Submit"]]]]]]])
